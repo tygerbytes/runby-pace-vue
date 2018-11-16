a78560be049a7988b00e5deaa9217150b77d8167
Ty Walls <tygerbytes@tygertec.com>
2018-11-16 14:13:21 -0500

    Change bootswatch theme

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 
 import BootstrapVue from 'bootstrap-vue';
-// Using "United" bootswatch
+// Using bootswatch
 import '@/assets/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
